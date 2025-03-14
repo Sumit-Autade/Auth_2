3b5f48699306f30c93134bf90f4133adde8ce797
Sumit Autade <128409006+Sumit-Autade@users.noreply.github.com>
2025-03-14 13:03:53 +0530

    gradient Navbar.jsx

--- Client/src/utils/Navbar.jsx
+++ Client/src/utils/Navbar.jsx
@@ -3,7 +3,7 @@ import logo from "../assets/logo.jpeg";
 
 const Navbar = () => {
   return (
-    <nav className="rounded-lg bg-gradient-to-r bg-blue-100 text-white p-4 flex items-center justify-between shadow-xl">
+    <nav className="rounded-lg bg-gradient-to-r from-[#fff0f5] via-[#f0f8ff] to-[#f5f5f5] text-gray-800 p-4 flex items-center justify-between">
       <div className="flex items-center gap-4">
         <img src={logo} alt="Logo" className="h-12 w-auto rounded-md shadow-md" />
         <span className="text-2xl font-bold tracking-wide"></span>
